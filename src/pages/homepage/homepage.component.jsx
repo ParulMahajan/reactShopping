9eaa7899f8f4bf93f462760b6ba7a8ad6aa0391f
Parul Mahajan <pmahajan@maxbit.co.th>
2022-02-04 11:15:45 +0700

    first commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+import Directory from "../../component/directory/directory.component";
+import './homepage.styles.scss'
+const HomePage = () => (
+
+    <Directory></Directory>
+);
+
+export default HomePage;
